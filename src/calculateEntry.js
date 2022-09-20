5eb287a6cc16b846b394208c19b396fa8a09b7aa
Gustavo <unidragorn@gmail.com>
2022-09-20 20:03:45 -0300

    adiciona testes handlerElephants e função do requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || entrants.length === undefined) return 0;
+  return countEntrants(entrants).senior * 24.99 + countEntrants(entrants).adult * 49.99
+   + countEntrants(entrants).child * 20.99;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,39 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('count', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+
+  test('names', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+
+  test('averageAge', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+
+  test('location', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+
+  test('popularity', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+
+  test('availability', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+
+  test('undefined se não tiver parâmetro', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+
+  test('Retorna "Parâmetro inválido, é necessário uma string" se parâmetro não for string', () => {
+    expect(handlerElephants({})).toEqual('Parâmetro inválido, é necessário uma string');
+  });
+
+  test('null se parâmetro for diferente do esperado', () => {
+    expect(handlerElephants('Melzinho com Torresmo')).toBeNull();
+  });
+});
