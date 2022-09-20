7076ef1f5fd97ba9cb33c3006489f5416573530a
Gustavo <unidragorn@gmail.com>
2022-09-20 18:31:47 -0300

    implementa função requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((i, spe) => Object.assign(i, { [spe.name]: spe.residents.length }), {});
+  }
+  const residentList = species.find((spe) => spe.name === animal.specie).residents;
+  if (!animal.sex) return residentList.length;
+  return residentList.filter((resident) => resident.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
