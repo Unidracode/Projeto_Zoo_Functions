fed8d2083cfc15918cb9ea509c91291a9eb826bc
Gustavo <unidragorn@gmail.com>
2022-09-20 17:18:02 -0300

    implementa função requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  const employee = employeeName;
+  return employees.find((name) => name.firstName === employee || name.lastName === employee);
 }
 
 module.exports = getEmployeeByName;
