efa123de198736078adfc99be16914ecb5f3f2df
Gustavo <unidragorn@gmail.com>
2022-09-20 21:35:38 -0300

    adiciona função requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animal = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const { name, sex, age } = species.find((spec) => spec.id === animal)
+    .residents.sort((b, c) => c.age - b.age)[0];
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
